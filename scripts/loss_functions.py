acc9970196546d916fbe13552bb48451697db112
navdeepkaushish <navdeepkaushish@gmail.com>
2024-10-15 14:46:51 +0200

    Add files via upload

---
+++ scripts/loss_functions.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 21 10:43:22 2023
+
+@author: Navdeep Kumar
+"""
+
+import torch
+import torch.nn as nn
+
+
+ALPHA = 0.2
+BETA = 0.8
+
+#======================== Dice loss ==========================================
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        
+        return 1 - dice
+
+#=================== DiceBCE Loss =============================================
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        BCE = nn.BCELoss()
+        bce_loss = BCE(inputs, targets)
+        Dice_BCE = bce_loss + dice_loss
+        
+        return Dice_BCE
+
+
+#===================== IoU/Jaccard loss =======================================
+class IoULoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(IoULoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #intersection is equivalent to True Positive count
+        #union is the mutually inclusive area of all labels & predictions 
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection 
+        
+        IoU = (intersection + smooth)/(union + smooth)
+                
+        return 1 - IoU
+#==================== Focal loss =============================================
+class FocalLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(FocalLoss, self).__init__()
+
+    def forward(self, inputs, targets, alpha=0.8, gamma=2, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #first compute binary cross-entropy 
+        bce_loss = nn.BCELoss()
+        BCE = bce_loss(inputs, targets)
+        BCE_EXP = torch.exp(-BCE)
+        focal_loss = alpha * (1-BCE_EXP)**gamma * BCE
+                       
+        return focal_loss
+
+#=============== Tversky loss =================================================
+
+class TverskyLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(TverskyLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1, alpha=ALPHA, beta=BETA):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #True Positives, False Positives & False Negatives
+        TP = (inputs * targets).sum()    
+        FP = ((1-targets) * inputs).sum()
+        FN = (targets * (1-inputs)).sum()
+       
+        Tversky = (TP + smooth) / (TP + alpha*FP + beta*FN + smooth)  
+        
+        return 1 - Tversky
+#================ FocalTversky loss ===========================================
+
+class FocalTverskyLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(FocalTverskyLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1, alpha=0.5, beta=0.5, gamma=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #True Positives, False Positives & False Negatives
+        TP = (inputs * targets).sum()    
+        FP = ((1-targets) * inputs).sum()
+        FN = (targets * (1-inputs)).sum()
+        
+        Tversky = (TP + smooth) / (TP + alpha*FP + beta*FN + smooth)  
+        FocalTversky = (1 - Tversky)**gamma
+                       
+        return FocalTversky
+    
+#================== Combo loss =============================================
+ALPHA = 0.5 # < 0.5 penalises FP more, > 0.5 penalises FN more
+CE_RATIO = 0.5 #weighted contribution of modified CE loss compared to Dice loss
+eps=1e-10
+class ComboLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(ComboLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1, alpha=ALPHA, beta=BETA):
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #True Positives, False Positives & False Negatives
+        intersection = (inputs * targets).sum()    
+        dice = (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+        
+        inputs = torch.clamp(inputs, eps, 1.0 - eps)       
+        out = - (ALPHA * ((targets * torch.log(inputs)) + ((1 - ALPHA) * (1.0 - targets) * torch.log(1.0 - inputs))))
+        weighted_ce = out.mean(-1)
+        combo = (CE_RATIO * weighted_ce) - ((1 - CE_RATIO) * dice)
+        
+        return combo
+    
+#==============================================================================
+    
---
+++ scripts/unet_model.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 27 10:59:39 2023
+
+@author: Navdeep Kumar
+"""
+
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+
+
+class Double_conv(nn.Module):
+    def __init__(self, in_channels, out_channels,mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+            
+        self.double_conv = nn.Sequential(nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding='same'),
+                                         nn.ReLU(inplace=True),
+                                         nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding='same'),
+                                         nn.ReLU(inplace=True)
+                                         #nn.BatchNorm2d(out_channels)
+                                         )
+            
+    def forward(self, x):
+        x = self.double_conv(x)
+        
+        return x
+            
+        
+
+class Downsample_block(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None, pooling_on=True):
+        super().__init__()
+        self.double_conv = Double_conv(in_channels, out_channels)
+        self.pooling_on = pooling_on
+        self.max_pool = nn.MaxPool2d(2, stride=2)
+            
+    def forward(self,x):
+        x = self.double_conv(x)
+        skip = x
+        if self.pooling_on:
+            x = self.max_pool(x)
+        
+        return x, skip
+    
+class Upsample_block(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        
+        self.upsample = nn.Sequential(nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
+                                      nn.ReLU(inplace=True),
+                                      nn.BatchNorm2d(out_channels)
+                                      
+                                     )
+        self.conv = Double_conv(in_channels, out_channels)
+       
+    def forward(self, x, skip):
+        
+        x = self.upsample(x)
+        x = torch.cat([x, skip], dim=1)
+        x  = self.conv(x)
+        return x
+        
+        
+        
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+    
+    
+class UNet(nn.Module):
+    def __init__(self, in_channels, n_masks):
+        
+        super().__init__()
+        #Downsample blocks
+        self.down1 = Downsample_block(in_channels, 64)
+        self.down2 = Downsample_block(64, 128)
+        self.down3 = Downsample_block(128, 256)
+        self.down4 = Downsample_block(256, 512)
+        self.down5 = Downsample_block(512, 1024, pooling_on=False)
+        #Upsample blocks
+        self.up1 = Upsample_block(1024, 512)
+        self.up2 = Upsample_block(512, 256)
+        self.up3 = Upsample_block(256, 128)
+        self.up4 = Upsample_block(128, 64)
+        self.outconv = OutConv(64, n_masks)
+        
+    def forward(self,x):
+        x, skip1 = self.down1(x)
+        x, skip2 = self.down2(x)
+        x, skip3 = self.down3(x)
+        x, skip4 = self.down4(x)
+        x, _ = self.down5(x)
+        #print(x.shape)
+        x = self.up1(x,skip4)
+        #print(x.shape, skip4.shape)
+        x = self.up2(x,skip3)
+        #print(x.shape, skip3.shape)
+        x = self.up3(x,skip2)
+        #print(x.shape, skip2.shape)
+        x = self.up4(x,skip1)
+        #print(x.shape, skip1.shape)
+        out = self.outconv(x)
+        #print(out.shape)
+        return out
+    
+#x = torch.randn(1,3,256,256)
+#model = UNet(3,25)
+       
+       
+        
